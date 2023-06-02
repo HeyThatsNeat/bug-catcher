8608e63be3f8c34adfe4b7afecd19a04db7cfd31
Hunter Eckhoff <Phantazm777@verizon.net>
2023-06-02 13:42:05 -0400

    add bug detail url, view, html, and css

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 path('', views.home, name='home'),
 path('about/', views.about, name='about'),
 path('bugs/', views.bug_index, name='bug-index'),
+path('bugs/<int:bug_id>/', views.bug_detail, name='bug-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,6 +6,10 @@ def bug_index(request):
   bugs = Bug.objects.all()
   return render(request, 'bugs/index.html', { 'bugs': bugs })
 
+def bug_detail(request, bug_id):
+  bug = Bug.objects.get(id=bug_id)
+  return render(request, 'bugs/detail.html', { 'bug': bug })
+
 def home(request):
   return render(request, 'home.html')
 
