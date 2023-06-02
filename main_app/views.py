b60bf92d73fc0c4abbd903f13f414050c8923c53
Hunter Eckhoff <Phantazm777@verizon.net>
2023-06-02 13:35:58 -0400

    add admin.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,9 @@
 from django.shortcuts import render
+from .models import Bug
 
-
-class Bug:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-bugs = [
-  Bug('Lolo', 'tabby', 'Kinda rude.', 3),
-  Bug('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Bug('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Bug('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
 
 def bug_index(request):
+  bugs = Bug.objects.all()
   return render(request, 'bugs/index.html', { 'bugs': bugs })
 
 def home(request):
