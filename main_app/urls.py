fb9f289426e338390e7314c1999b3273166766d5
Hunter Eckhoff <Phantazm777@verizon.net>
2023-06-02 14:12:41 -0400

    add sign up for new users

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ path('', views.Home.as_view(), name='home'),
 path('about/', views.about, name='about'),
 path('bugs/', views.bug_index, name='bug-index'),
 path('bugs/<int:bug_id>/', views.bug_detail, name='bug-detail'),
+path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Bug
 
 
 def bug_index(request):
-  bugs = Bug.objects.all()
+  bugs = Bug.objects.filter(user=request.user)
   return render(request, 'bugs/index.html', { 'bugs': bugs })
 
 def bug_detail(request, bug_id):
@@ -13,6 +15,28 @@ def bug_detail(request, bug_id):
 
 class Home(LoginView):
   template_name = 'home.html'
+
+# class BugCreate(CreateView):
+#   model = Bug
+#   fields = ['name', 'breed', 'description', 'age']
+  
+#   def form_valid(self, form):
+#     form.instance.user = self.request.user
+#     return super().form_valid(form)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('cat-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
 
 def about(request):
   return render(request, 'about.html')
